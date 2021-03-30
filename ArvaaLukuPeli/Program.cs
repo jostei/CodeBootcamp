1f152c0e1e58e28fdabc598234ea30fafe27d247
jostei <jose.teittinen@gmail.com>
2021-03-30 15:06:05 +0300

    Kommentoitu ja viimeistelty

--- ArvaaLukuPeli/Program.cs
+++ ArvaaLukuPeli/Program.cs
@@ -1,35 +1,43 @@
-﻿using System;
+﻿// Arvaa luku-peli, toteutus: jostei
+using System;
 
 namespace ArvaaLukuPeli
 {
     class Program
     {
+        const int maksimiLuku = 20;
+        const int kierrostenMaara = 3;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Arvotaan luku väliltä 1-20...");
+            Console.WriteLine($"Arvotaan luku väliltä 1-{maksimiLuku}...");
 
+            // Arvotaan satunnaisluku väliltä [1,maksimiLuku+1]
             Random rnd = new Random();
-            int arvattavaLuku = rnd.Next(21);
-            int kierrostenMaara = 3;
+            int arvattavaLuku = rnd.Next(1,maksimiLuku+1);
 
-            Console.WriteLine("Saat kolme arvausta. Arvaa Luku.");
+            Console.WriteLine($"Saat {kierrostenMaara} arvausta. Arvaa Luku.");
 
             int kierros = 0;
-            int arvaus = 0;
+            int arvaus;
             int[] arvatutLuvut = new int[kierrostenMaara];
             bool lukuArvattu = false;
 
+            // kysytään käyttäjältä lukua kierrostenMaara kertoja
             while(!lukuArvattu & kierros < kierrostenMaara){
                 arvaus = Int32.Parse(Console.ReadLine());
                 arvatutLuvut[kierros] = arvaus;
                 kierros++;
 
+                // Arvattu luku on liian pieni
                 if(arvaus > arvattavaLuku & kierros < kierrostenMaara){
                     Console.WriteLine("Liian suuri, yritä uudestaan!");
                 }
+                // Arvattu luku on liian suuri
                 if(arvaus < arvattavaLuku & kierros < kierrostenMaara){
                     Console.WriteLine("Liian pieni, yritä uudestaan!");
                 }
+                // Arvaus oikein, lopetetaan lukujen kysely
                 if(arvaus == arvattavaLuku){
                     Console.WriteLine("Oikein!!");
                     lukuArvattu = true;
@@ -37,15 +45,16 @@ namespace ArvaaLukuPeli
                 }
             }
 
+            // Jose luku arvattu, paljastetaan oikea luku ja esitellään pelaajan arvaukset
             if(!lukuArvattu){
-                Console.WriteLine("Et arvannut. Luku oli: "+arvattavaLuku+".");
+                Console.WriteLine($"Et arvannut. Luku oli: {arvattavaLuku}.");
                 Console.Write("Arvaamasi luvut: ");
                 foreach (int luku in arvatutLuvut)
                 {
                     Console.Write(luku+" ");                    
                 }
             }
-            Console.WriteLine("Peli on päättynyt.");
+            Console.WriteLine("\nPeli on päättynyt.");
         }
     }
 }
