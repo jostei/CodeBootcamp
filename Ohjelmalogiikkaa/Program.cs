58f49fd43c17d357e0388629f3d176f9ed690542
jostei <jose.teittinen@gmail.com>
2021-03-30 11:36:04 +0300

    "Lisätty muuttujia ja tuloste"

--- Ohjelmalogiikkaa/Program.cs
+++ Ohjelmalogiikkaa/Program.cs
@@ -7,6 +7,18 @@ namespace Ohjelmalogiikkaa
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            int luku = 2;
+            int toinenluku;
+
+            toinenluku = 4;
+            const int pysyvaluku = 3;
+
+            Console.Write("O_o");
+            {
+                
+            }
+
         }
     }
 }
