24bf7c9ed7ad79264e8be77f3fe4b3197269518c
jostei <jose.teittinen@gmail.com>
2021-04-01 11:39:57 +0300

    Lisätty CV-sivu

--- DotNet/AspNetMvcDemo/Controllers/HomeController.cs
+++ DotNet/AspNetMvcDemo/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace AspNetMvcDemo.Controllers
             return View();
         }
 
+        public IActionResult OmaCV()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
