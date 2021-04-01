e8c9e590498be329ca62bbd3431567a4e6668f1c
jostei <jose.teittinen@gmail.com>
2021-04-01 10:34:24 +0300

    Lisätty ASP.NET MVC projekti

---
+++ DotNet/AspNetMvcDemo/wwwroot/js/summaus.js
@@ -0,0 +1,5 @@
+let luku1 = parseInt(document.getElementById("input1").value);
+let luku2 = parseInt(document.getElementById("input2").value);
+
+console.log("JS saavutettu!");
+document.getElementById("output").innerHTML = parseInt(luku1)+parseInt(luku2);
