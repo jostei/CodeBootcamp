139ff119947a86ba81b1ee995f7d269ae8e66e5d
jostei <jose.teittinen@gmail.com>
2021-04-01 12:21:31 +0300

    Lisätty Bootstrap-muotoilua

--- DotNet/AspNetMvcDemo/wwwroot/js/summaus.js
+++ DotNet/AspNetMvcDemo/wwwroot/js/summaus.js
@@ -1,5 +1,9 @@
-let luku1 = parseInt(document.getElementById("input1").value);
-let luku2 = parseInt(document.getElementById("input2").value);
+$("#sumButton").click(
+    function()
+    {
+    const luku1 = $("#input1").val();
+    const luku2 = $("#input2").val();
 
-console.log("JS saavutettu!");
-document.getElementById("output").innerHTML = parseInt(luku1)+parseInt(luku2);
+    $("#output")[0].innerHTML = parseInt(luku1)+parseInt(luku2);
+    }
+);
