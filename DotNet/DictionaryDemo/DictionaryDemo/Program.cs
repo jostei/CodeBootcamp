a053b0e7a0447d8c46ba3c84d14bffba12a85e7c
jostei <jose.teittinen@gmail.com>
2021-04-09 14:28:44 +0300

    Lisätty luontokirja

--- DotNet/DictionaryDemo/DictionaryDemo/Program.cs
+++ DotNet/DictionaryDemo/DictionaryDemo/Program.cs
@@ -7,23 +7,46 @@ namespace DictionaryDemo
     {
         static void Main(string[] args)
         {
-            Dictionary<int, elain> kirja = new();
+            Dictionary<int, Elain> kirja = new();
 
             // Lisätään eläimiä listaan
-            kirja.Add(1001, new elain("koira"));
-            kirja.Add(1002, new elain("kissa"));
-            kirja.Add(1003, new elain("lehmä"));
+            kirja.Add(1001, new Elain("koira"));
+            kirja.Add(1002, new Elain("kissa"));
+            kirja.Add(1003, new Elain("lehmä"));
 
             Console.WriteLine(kirja[1003].Laji);
             // > lehmä
+
+            Dictionary<Uhanalaisuus, List<Elain>> Luontokirja = new();
+
+            Luontokirja.Add(Uhanalaisuus.EiUhanalainen, new List<Elain>());
+            Luontokirja.Add(Uhanalaisuus.Vaarassa, new List<Elain>());
+            Luontokirja.Add(Uhanalaisuus.Uhanalainen, new List<Elain>());
+            Luontokirja.Add(Uhanalaisuus.Fiktiivinen, new List<Elain>());
+
+            Luontokirja[Uhanalaisuus.EiUhanalainen].Add(new Elain("Kissa"));
+            Luontokirja[Uhanalaisuus.EiUhanalainen].Add(new Elain("Koira"));
+            Luontokirja[Uhanalaisuus.Vaarassa].Add(new Elain("Saimaannorppa"));
+            Luontokirja[Uhanalaisuus.Uhanalainen].Add(new Elain("Siperiantiikeri"));
+            Luontokirja[Uhanalaisuus.Fiktiivinen].Add(new Elain("Yksisarvinen"));
+            Luontokirja[Uhanalaisuus.Fiktiivinen].Add(new Elain("Lohikäärme"));
+
+
         }
     }
+    public enum Uhanalaisuus
+    {
+        EiUhanalainen,
+        Vaarassa,
+        Uhanalainen,
+        Fiktiivinen
+    }
 
-    class elain
+    class Elain
     {
         public string Laji { get; set; }
     
-        public elain(string nimi)
+        public Elain(string nimi)
         {
             Laji = nimi;
         }
