ad1e2325743dfe1ada1f3ae526279f90e48433dc
jostei <jose.teittinen@gmail.com>
2021-04-09 13:42:39 +0300

    Lisätty sanakirjademo

---
+++ DotNet/DictionaryDemo/DictionaryDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DictionaryDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, elain> kirja = new();
+
+            // Lisätään eläimiä listaan
+            kirja.Add(1001, new elain("koira"));
+            kirja.Add(1002, new elain("kissa"));
+            kirja.Add(1003, new elain("lehmä"));
+
+            Console.WriteLine(kirja[1003].Laji);
+            // > lehmä
+        }
+    }
+
+    class elain
+    {
+        public string Laji { get; set; }
+    
+        public elain(string nimi)
+        {
+            Laji = nimi;
+        }
+    }
+}
