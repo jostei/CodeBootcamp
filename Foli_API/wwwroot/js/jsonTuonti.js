f3449a4c12e3f1b5962f1293c8bb03bdda7c176c
jostei <jose.teittinen@gmail.com>
2021-04-09 09:20:21 +0300

    Fölidemo luotu

---
+++ Foli_API/wwwroot/js/jsonTuonti.js
@@ -0,0 +1,12 @@
+﻿// Noutaa JSON muotoista infoa Fölistä
+
+$("#noutoNappi").click(
+    async function () {
+
+        const apiURL = "http://data.foli.fi/citybike/pretty";
+
+        fetch(apiURL)
+            .then(response => response.json())
+            .then(data => console.log(data));
+    }
+)
