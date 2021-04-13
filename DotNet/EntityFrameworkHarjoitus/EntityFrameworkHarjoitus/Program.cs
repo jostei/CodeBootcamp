b4d718b080ea6fb374cd05f9e4f3a6447ac99a0e
jostei <jose.teittinen@gmail.com>
2021-04-13 15:55:45 +0300

    Lisätty Entity framework-malli tietokantakyselystä

---
+++ DotNet/EntityFrameworkHarjoitus/EntityFrameworkHarjoitus/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using EntityFrameworkHarjoitus.Models;
+
+namespace EntityFrameworkHarjoitus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NorthwindContext konteksti = new();
+            foreach (var asiakas in konteksti.Customers)
+            {
+                if(asiakas.Country == "Finland")
+                {
+                    Console.WriteLine(asiakas.CompanyName);
+                }
+            }
+        }
+    }
+}
