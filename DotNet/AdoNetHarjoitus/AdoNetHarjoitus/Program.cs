835644cae6357305f22237ae977c4a86dc7a6bbd
jostei <jose.teittinen@gmail.com>
2021-04-13 10:51:14 +0300

    Toteutettu yhteyden muodostus ja query

--- DotNet/AdoNetHarjoitus/AdoNetHarjoitus/Program.cs
+++ DotNet/AdoNetHarjoitus/AdoNetHarjoitus/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// Connects to a local database and executes and prints a query
+
+using System;
+using System.Data.SqlClient;
 
 namespace AdoNetHarjoitus
 {
@@ -6,7 +9,31 @@ namespace AdoNetHarjoitus
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string myServerAddress = @"localhost\SQLEXPRESS";
+            string myDataBase = "Northwind";
+            
+            string connectionString = $"Server={myServerAddress};Database={myDataBase};Trusted_Connection=True;";
+            string queryString = "SELECT * FROM Customers WHERE Country='Finland'";
+
+            SqlConnection sc = new(connectionString);
+            SqlCommand command = new(queryString, sc);
+
+            // Opening connection
+            sc.Open();
+
+            Console.WriteLine($"Connected to database {myDataBase}.");
+            SqlDataReader reader = command.ExecuteReader();
+
+            // Print all found company names
+            while (reader.Read())
+            {
+                string companyName = reader["CompanyName"].ToString();
+                Console.WriteLine(companyName);
+            }
+
+            // Closing connection
+            sc.Close();
+            Console.WriteLine("Connection closed.");
         }
     }
 }
